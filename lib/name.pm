a939277264e0606feefc8f99376f8b194dfb84cc
Bernhard Graf <augensalat@gmail.com>
2022-01-31 18:30:47 +0100

    Initial commit
    Generated with
    
        module-starter --module=name

---
+++ lib/name.pm
@@ -0,0 +1,105 @@
+package name;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+name - The great new name!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use name;
+
+    my $foo = name->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Bernhard Graf, C<< <graf at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-name at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=name>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc name
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=name>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/name>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/name>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Bernhard Graf.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of name
